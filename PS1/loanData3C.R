079e8982c073e2665e53b944ff4a9e2115398aa4
Aina Lopez - student account <aina.lopez@barcelonagse.eu>
2016-01-14 10:09:46 +0100

    Update loanData3C.R

--- PS1/loanData3C.R
+++ PS1/loanData3C.R
@@ -8,35 +8,35 @@ loadfonts()
 
 # create small wrapper functions
 sigmaXY <- function(rho, sdX, sdY) {
-    covTerm <- rho * sdX * sdY
-    VCmatrix <- matrix(c(sdX^2, covTerm, covTerm, sdY^2), 2, 2, byrow = TRUE)
-    
-    return(VCmatrix)
+  covTerm <- rho * sdX * sdY
+  VCmatrix <- matrix(c(sdX^2, covTerm, covTerm, sdY^2), 2, 2, byrow = TRUE)
+ 
+  return(VCmatrix) 
 }
 
-genBVN <- function(n = 1, seed = NA, muXY=c(0,1), sigmaXY=diag(2)) {
-    if(!is.na(seed)) set.seed(seed)
-    rdraws <- rmvnorm(n, mean = muXY, sigma = sigmaXY)
-    return(rdraws)
+genBVN <- function(n = 1, seed = NA, muXY=c(0,1), sigmaXY=diag(2)) { 
+  if(!is.na(seed)) set.seed(seed)
+  rdraws <- rmvnorm(n, mean = muXY, sigma = sigmaXY) 
+  return(rdraws)
 }
 
 # loanData function modified in order to add a third class: undecided
 loanData3 <- function(noApproved, noDenied, noUndecided, muApproved, muDenied, muUndecided, sdApproved,
-sdDenied, sdUndecided, rhoApproved, rhoDenied, rhoUndecided, seed=1111) {
-    
-    sigmaApproved <- sigmaXY(rho=rhoApproved, sdX=sdApproved[1], sdY=sdApproved[2])
-    sigmaDenied <- sigmaXY(rho=rhoDenied, sdX=sdDenied[1], sdY=sdDenied[2])
-    sigmaUndecided <- sigmaXY(rho=rhoUndecided, sdX=sdUndecided[1], sdY=sdUndecided[2])
-    approved <- genBVN(noApproved, muApproved, sigmaApproved, seed = seed)
-    denied <- genBVN(noDenied, muDenied, sigmaDenied, seed = seed+1)
-    undecided <- genBVN(noUndecided, muUndecided, sigmaUndecided, seed = seed+2)
-    loanDf <- as.data.frame(rbind(approved,denied, undecided))
-    deny <- c(rep("Approved", noApproved), rep("Denied", noDenied), rep("Undecided", noUndecided))
-    target = c(rep(0, noApproved), rep(1, noDenied), rep(2, noUndecided))
-    loanDf <- data.frame(loanDf, deny, target)
-    colnames(loanDf) <- c("PIratio", "solvency", "deny", "target")
-    
-    return(loanDf)
+                     sdDenied, sdUndecided, rhoApproved, rhoDenied, rhoUndecided, seed=1111) {
+  
+  sigmaApproved <- sigmaXY(rho=rhoApproved, sdX=sdApproved[1], sdY=sdApproved[2])
+  sigmaDenied <- sigmaXY(rho=rhoDenied, sdX=sdDenied[1], sdY=sdDenied[2])
+  sigmaUndecided <- sigmaXY(rho=rhoUndecided, sdX=sdUndecided[1], sdY=sdUndecided[2])
+  approved <- genBVN(noApproved, muApproved, sigmaApproved, seed = seed)
+  denied <- genBVN(noDenied, muDenied, sigmaDenied, seed = seed+1)
+  undecided <- genBVN(noUndecided, muUndecided, sigmaUndecided, seed = seed+2)
+  loanDf <- as.data.frame(rbind(approved,denied, undecided))
+  deny <- c(rep("Approved", noApproved), rep("Denied", noDenied), rep("Undecided", noUndecided)) 
+  target = c(rep(0, noApproved), rep(1, noDenied), rep(2, noUndecided))
+  loanDf <- data.frame(loanDf, deny, target)
+  colnames(loanDf) <- c("PIratio", "solvency", "deny", "target") 
+  
+  return(loanDf)
 }
 
 # Generate the 3-class dataset
@@ -44,24 +44,24 @@ noApproved  <- 50
 noDenied    <- 50
 noUndecided <- 50
 
-LoanD3 <- loanData3(noApproved, noDenied, noUndecided, c(4, 150), c(10,100), c(7,210) ,
-c(1,20), c(2,30), c(1,5), -0.1, 0.6, 0.1, 1221)
+LoanD3 <- loanData3(noApproved, noDenied, noUndecided, c(4, 150), c(10,100), c(8,230) ,
+                    c(1,20), c(2,30), c(0.5,5), -0.1, 0.6, 0.5, 1221)
 
 # add target variable
 LoanD3C <- cbind(LoanD3,
-target1 = c(rep(0, noApproved), rep(1, noDenied), rep(0, noUndecided)),
-target2 = c(rep(1, noApproved), rep(0, noDenied), rep(0, noUndecided)),
-target3 = c(rep(0, noApproved), rep(0, noDenied), rep(1, noUndecided))
-)
+                target1 = c(rep(0, noApproved), rep(1, noDenied), rep(0, noUndecided)),
+                target2 = c(rep(1, noApproved), rep(0, noDenied), rep(0, noUndecided)),
+                target3 = c(rep(0, noApproved), rep(0, noDenied), rep(1, noUndecided))
+                )
 
 # analytical solutions
 X <- as.matrix(cbind(ind=rep(1,nrow(LoanD3C)),
-LoanD3C[,c("PIratio","solvency")]))
+                     LoanD3C[,c("PIratio","solvency")]))
 
 Y <- cbind(denied_prob = c(rep(0, noApproved), rep(1, noDenied), rep(0, noUndecided)),
-approved_prob = c(rep(1, noApproved), rep(0, noDenied), rep(0, noUndecided)),
-undecided_prob = c(rep(0, noApproved), rep(0, noDenied), rep(1, noUndecided))
-)
+           approved_prob = c(rep(1, noApproved), rep(0, noDenied), rep(0, noUndecided)),
+           undecided_prob = c(rep(0, noApproved), rep(0, noDenied), rep(1, noUndecided))
+           )
 
 weightsOptim <- solve(t(X) %*% X) %*% t(X) %*% Y
 
@@ -74,7 +74,7 @@ undecided <- (predictions==apply(predictions, 1, max))[,3]
 
 predictedLabels <- ifelse(denied, "Denied", "Approved")
 predictedLabels[undecided == TRUE] <- "Undecided"
-
+  
 
 # Create the final dataframe: dataset + predictions for each class and final decission
 LoanD3 <- cbind(LoanD3, predictions, predictedLabels)
@@ -98,16 +98,15 @@ slope3     <- (weightsOptim[3,3] - weightsOptim[3,2])/(weightsOptim[2,2] - weigh
 # plot
 pdf("discFunction3C.pdf", family = "Tahoma", width=5, height=5)
 ggplot(data = LoanD3,
-aes(x = solvency, y = PIratio, colour = deny, fill = deny)) +
-geom_point() +
-xlab("solvency") +
-ylab("PIratio") +
-ggtitle("Loan data and decision boundaries") +
-theme_bw() +
-theme(text = element_text(family = "Tahoma")) +
-geom_abline(intercept = intercept1, slope = slope1, colour = "yellow") +
-geom_abline(intercept = intercept2, slope = slope2, colour = "cyan") +
-geom_abline(intercept = intercept3, slope = slope3, colour = "magenta")
+       aes(x = solvency, y = PIratio, colour = deny, fill = deny)) + 
+  geom_point() + 
+  xlab("solvency") + 
+  ylab("PIratio") + 
+  ggtitle("Loan data and decision boundaries") + 
+  theme_bw() + 
+  theme(text = element_text(family = "Tahoma")) +
+  geom_abline(intercept = intercept1, slope = slope1, colour = "yellow") + 
+  geom_abline(intercept = intercept2, slope = slope2, colour = "cyan") + 
+  geom_abline(intercept = intercept3, slope = slope3, colour = "magenta") 
 dev.off()
 embed_fonts("discFunction3C.pdf")
-
